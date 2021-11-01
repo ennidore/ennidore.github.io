50343ef6c089fa14b2f4f1cfbd88cde3fd670fa8
one <one@place.com>
2021-11-01 18:48:22 +0100

    Dodavanje svih fileova

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".ham-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
